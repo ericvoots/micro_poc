088f1deb1f7b83f1000d317cea12ee75518d64c7
Eric Voots <stonks@python.micropredictions.stonks.voots.org>
2022-03-20 21:16:40 +0000

    streams.py finished and added to call for it using main.py

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import celery
 
+from streams import streams
+
 '''
 this is where everything from streams, getting data, creating features and models will be called
 
 '''
+
+
+
+
+if __name__ == "__main__":
+           
+    # step 1 kind of every day or week or so make sure streams are updated
+    streamsdl = streams(config_file="config.toml")
+    
+    streamsdl.get_streams()
+
+
+
+# if not updating streams step 2, get data and features
+
+
+
+# step 3 feature selection if updating the model, default update schedule is once a week
+
+
+
+# step 4 model retrain if updating the model as well
+
+
+
--- streams.py
+++ streams.py
@@ -1,7 +1,10 @@
 import json
+import microprediction
 import pandas as pd
 
 import toml
+
+from microprediction import MicroReader
 
 '''
 used for getting streams to be used in the crawler, then saved into a json file.
@@ -15,20 +18,22 @@ class streams(object):
     def __init__(self, config_file):
         
         self.config = toml.load(config_file)
-        self.streams = []
-        
-        print(f"current setup is {self.config}")
+        self.streams = {}
+        self.mr = MicroReader()
         
     def get_streams(self):
+        """_summary_
+        this is the function used to update the streams and save them off to a json file
+        """
+        all_streams = self.mr.get_stream_names()
         
-        print(f'test {self.streams}')
-
-    
-if __name__ == "__main__":
-    
-    # can probably just make this an argument later with argparse and passed in from jenkins or something
-    get_streams = streams(config_file="config.toml")
-
-    
-    
-    
+        for stream in self.config['stream_prefixes']:
+            #self.streams.append([i for i in all_streams if i.startswith(stream)])
+            
+            self.streams[stream] = [i for i in all_streams if i.startswith(stream)]
+            
+         # save stream names into current folder with json, just change the folder to save to using config if needed
+        
+        with open("streams.json", "w") as outfile:
+            json.dump(self.streams, outfile)
+  
