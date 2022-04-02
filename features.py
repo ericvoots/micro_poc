5e9da929c23df98a527ff356ae8f958e54e97f29
Eric Voots <stonks@python.micropredictions.stonks.voots.org>
2022-04-02 20:38:22 +0000

    updated how streams were called to features

--- features.py
+++ features.py
@@ -18,22 +18,33 @@ class features(object):
         object (object): _description_ features class
     """
     
-    def __init__(self, config_file):
+    def __init__(self, config_file, stream):
         """_summary_
         init of the features class
         Args:
             config_file (string): _description_ location of the toml file with all the settings
+            stream (string): _description_ which stream is currently being used
+        """
+        self.stream = stream
+        self.config = toml.load(config_file)
+       
+        # maximum number of values to download
+        self.max_count = 900        
+        self.mr = MicroReader()
+    
+    
+    def check_max_datetime(self, datetime):
+        """_summary_
+        checks to be sure that the max date available in the time series is within the last 30 minutes UTC
+        later add a discord web hook
+        Args:
+            datetime (string/datetime): _description_
         """
         
-        self.config = toml.load(config_file)
-        # to limit the number of lags for feature creation
-        self.max_lag = self.config["max_lags"]
-        
-        # get the streams
-        stream = open("streams.json")
-        self.streams = json.load(stream)
-        
-        self.mr = MicroReader()
+    def ma_feature_calc(self):
+        """
+        creates some moving average based features based on lags and pct diff from current
+        """    
         
         
     def get_data(self):
@@ -44,30 +55,24 @@ class features(object):
         can just add a max to the config file if you want to limit the total number 
         """
         
-        print('getting data')
+        print('getting lagged values for stream {self.stream}')
         
-        # hard coding which two streams to only use for now
-        print(f"config file {self.config} ")
+                   
+        # add a step to check if data exists
         
-        #TODO remove this
-        stream_list = ["c2_rebalanced_95.json", "c2_rebalanced_55_mean.json"]
         
-        for stream in stream_list:
+        lagged_values, lagged_seconds = self.mr.get_lagged_values_and_times(name=self.stream,
+                                                                            count=self.max_count)
         
-            lagged_values, lagged_seconds = self.mr.get_lagged_values_and_times(stream)
-            values = list(reversed(lagged_values))
-            dt = [ datetime.fromtimestamp(s) for s in reversed(lagged_seconds)] 
-            df = pd.DataFrame(columns=['date', 'y'])
-            df['date'] = dt
-            df['y'] = values
+        values = list(reversed(lagged_values))
+        dt = [ datetime.fromtimestamp(s) for s in reversed(lagged_seconds)] 
+        self.df = pd.DataFrame(columns=['date', 'y'])
+        self.df['date'] = dt
+        self.df['y'] = values
+        
+        # append to previously downloaded data
+        print(self.df.tail(20))
             
-            print(f"\n\n\ntest {df.tail(20)}")
-            
-
-if __name__ == "__main__":
-    
-    features_class = features(config_file="config.toml")
-    
-    features_class.get_data()
-        
+            # look into processing times and max data set size per stream, for now limit to 5000
+       
     
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import celery
+import json
 
 from streams import streams
+from features import features
 
 '''
 this is where everything from streams, getting data, creating features and models will be called
@@ -11,16 +13,29 @@ this is where everything from streams, getting data, creating features and model
 
 
 if __name__ == "__main__":
-           
+    
+    #TODO add schedule here for updating the streams based on config file
     # step 1 kind of every day or week or so make sure streams are updated
     streamsdl = streams(config_file="config.toml")
     
     streamsdl.get_streams()
 
-
+        # get the streams
+    stream_json = open("streams.json")
+    stream_dict = json.load(stream_json)
+    
+    print(stream_dict)
+    
+    #TODO remove this, just a temp loop so I dont use all the streams
+    stream_list = ["c2_rebalanced_95.json", "c5_cardano.json"]
 
 # if not updating streams step 2, get data and features
-
+    for s in stream_list:
+        print(f"getting data and analyzing for stream:  {s}")
+        features_class = features(config_file="config.toml",
+                                  stream=s)
+    
+        features_class.get_data()
 
 
 # step 3 feature selection if updating the model, default update schedule is once a week
